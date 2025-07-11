58fcfd1c42515bf671572b2bccb10cf98cdf941c
Jai <hrconnect@aifirstops.com>
2025-07-10 23:46:34 -0400

    Fix: corrected LandingPage.scss import path

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import "../styles/LandingPage.scss";
+import "../Styles/LandingPage.scss";
 
 export default function LandingPage() {
   const features = [
